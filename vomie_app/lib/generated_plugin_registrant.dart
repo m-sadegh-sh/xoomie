4632c2ba0c5b2b6b19b2b72c34442cb0bde49687
Mohammad Sadegh Shad <m.sadegh.sh@gmail.com>
2022-04-24 13:44:09 +0430

    Add the bloc package

--- vomie_app/lib/generated_plugin_registrant.dart
+++ vomie_app/lib/generated_plugin_registrant.dart
@@ -7,6 +7,7 @@
 
 import 'package:firebase_auth_web/firebase_auth_web.dart';
 import 'package:firebase_core_web/firebase_core_web.dart';
+import 'package:flutter_keyboard_visibility_web/flutter_keyboard_visibility_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
@@ -14,5 +15,6 @@ import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 void registerPlugins(Registrar registrar) {
   FirebaseAuthWeb.registerWith(registrar);
   FirebaseCoreWeb.registerWith(registrar);
+  FlutterKeyboardVisibilityPlugin.registerWith(registrar);
   registrar.registerMessageHandler();
 }
