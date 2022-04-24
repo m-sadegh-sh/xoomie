ec7138273812465cd00497a065e1716d13d066c9
Mohammad Sadegh Shad <m.sadegh.sh@gmail.com>
2022-04-24 16:39:11 +0430

    Relocate localization files

--- vomie_app/lib/generated_plugin_registrant.dart
+++ vomie_app/lib/generated_plugin_registrant.dart
@@ -8,6 +8,7 @@
 import 'package:firebase_auth_web/firebase_auth_web.dart';
 import 'package:firebase_core_web/firebase_core_web.dart';
 import 'package:flutter_keyboard_visibility_web/flutter_keyboard_visibility_web.dart';
+import 'package:flutter_native_splash/flutter_native_splash_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
@@ -16,5 +17,6 @@ void registerPlugins(Registrar registrar) {
   FirebaseAuthWeb.registerWith(registrar);
   FirebaseCoreWeb.registerWith(registrar);
   FlutterKeyboardVisibilityPlugin.registerWith(registrar);
+  FlutterNativeSplashWeb.registerWith(registrar);
   registrar.registerMessageHandler();
 }
