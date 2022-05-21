00383bd3ba9d1280b720fdb5c64ea0310ef6bea7
Mohammad Sadegh Shad <m.sadegh.sh@gmail.com>
2022-05-21 18:31:53 +0430

    Expose duration and transitionType

--- xoomie_app/lib/src/base/widgets/default_value_switcher.dart
+++ xoomie_app/lib/src/base/widgets/default_value_switcher.dart
@@ -1,11 +1,16 @@
+import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
 import 'package:xoomie/src/base/widgets/default_widget_switcher.dart';
 
 class DefaultValueSwitcher<T> extends StatefulWidget {
+  final Duration duration;
+  final SharedAxisTransitionType transitionType;
   final T? value;
-  final Widget Function(T? value) builder;
+  final Widget? Function(T? value) builder;
 
   const DefaultValueSwitcher({
+    this.duration = const Duration(milliseconds: 500),
+    this.transitionType = SharedAxisTransitionType.vertical,
     required this.value,
     required this.builder,
     super.key,
@@ -42,6 +47,8 @@ class DefaultValueSwitcherState<T> extends State<DefaultValueSwitcher<T>> {
   @override
   Widget build(BuildContext context) {
     return DefaultWidgetSwitcher(
+      duration: widget.duration,
+      transitionType: widget.transitionType,
       showPrimary: firstValue != null,
       primary: widget.builder(firstValue),
       secondary: widget.builder(secondValue),
