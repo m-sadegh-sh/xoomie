04c1dbcdbd1db848cf53374d51be969187396b9b
Mohammad Sadegh Shad <m.sadegh.sh@gmail.com>
2022-05-21 16:59:48 +0430

    DI

--- vomie_app/lib/src/injection/root_services_registerar.dart
+++ vomie_app/lib/src/injection/root_services_registerar.dart
@@ -1,26 +1,73 @@
-import 'package:vomie/src/injection/container.dart';
-import 'package:vomie/src/router/bloc/router_bloc.dart';
-import 'package:vomie/src/splash/bloc/splash_bloc.dart';
+import 'package:zoomie/src/auth/bloc/auth_bloc.dart';
+import 'package:zoomie/src/auth/bloc/auth_repository.dart';
+import 'package:zoomie/src/injection/container.dart';
+import 'package:zoomie/src/router/bloc/router_bloc.dart';
+import 'package:zoomie/src/sign_in/bloc/sign_in_with_email_bloc.dart';
+import 'package:zoomie/src/sign_in/bloc/sign_in_repository.dart';
+import 'package:zoomie/src/sign_in/bloc/sign_in_with_google_bloc.dart';
+import 'package:zoomie/src/sign_out/bloc/sign_out_bloc.dart';
+import 'package:zoomie/src/sign_out/bloc/sign_out_repository.dart';
+import 'package:zoomie/src/sign_up/bloc/sign_up_repository.dart';
+import 'package:zoomie/src/sign_up/bloc/sign_up_with_email_bloc.dart';
+import 'package:zoomie/src/splash/bloc/splash_bloc.dart';
+import 'package:zoomie/src/theme/bloc/theme_bloc.dart';
 
 void registerRootServices() {
-  // registerSingleton<UrlFactoryBase>(
-  //   (_) => EnvironmentBasedUrlFactory(
-  //     values: dotenv.env,
-  //   ),
-  // );
+  registerSingleton(
+    (_) => ThemeBloc(),
+  );
 
   registerSingleton(
     (_) => RouterBloc(),
   );
 
-  // registerSingleton(
-  //   (c) => AuthBloc(routerBloc: c(), wordsBloc: c()),
-  // );
+  registerFactory<AuthRepositoryBase>(
+    (c) => FirebaseAuthRepository(),
+  );
 
-  registerSingleton(
-    (c) => SplashBloc(
-      routerBloc: c(),
-      // authBloc: c(),
+  registerFactory(
+    (c) => AuthBloc(
+      repository: c(),
+    ),
+  );
+
+  registerFactory(
+    (c) => SplashBloc(),
+  );
+
+  registerFactory<SignUpRepositoryBase>(
+    (c) => FirebaseSignUpRepository(),
+  );
+
+  registerFactory(
+    (c) => SignUpWithEmailBloc(
+      repository: c(),
+    ),
+  );
+
+  registerFactory<SignInRepositoryBase>(
+    (c) => FirebaseSignInRepository(),
+  );
+
+  registerFactory(
+    (c) => SignInWithEmailBloc(
+      repository: c(),
+    ),
+  );
+
+  registerFactory(
+    (c) => SignInWithGoogleBloc(
+      repository: c(),
+    ),
+  );
+
+  registerFactory<SignOutRepositoryBase>(
+    (c) => FirebaseSignOutRepository(),
+  );
+
+  registerFactory(
+    (c) => SignOutBloc(
+      repository: c(),
     ),
   );
 }
