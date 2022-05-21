1f32e841d70c69df41868223102f85789a429a54
Mohammad Sadegh Shad <m.sadegh.sh@gmail.com>
2022-05-21 17:00:22 +0430

    Main app

--- vomie_app/lib/main.dart
+++ vomie_app/lib/main.dart
@@ -1,16 +1,36 @@
+import 'package:equatable/equatable.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:vomie/src/app.dart';
-import 'package:vomie/src/base/bloc/app_bloc_observer.dart';
-import 'package:vomie/src/injection/root_services_registerar.dart';
+import 'package:flutter/services.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:zoomie/firebase_options.dart';
+import 'package:zoomie/src/app/views/app_container.dart';
+import 'package:zoomie/src/base/bloc/app_bloc_observer.dart';
+import 'package:zoomie/src/injection/root_services_registerar.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+
+  Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   registerRootServices();
 
+  EquatableConfig.stringify = true;
+
+  final storage = await HydratedStorage.build(
+    storageDirectory: await getApplicationDocumentsDirectory(),
+  );
+
   BlocOverrides.runZoned(
-    () => runApp(const App()),
+    () => HydratedBlocOverrides.runZoned(
+      () => runApp(const AppContainer()),
+      storage: storage,
+    ),
     blocObserver: AppBlocObserver(),
   );
 }
